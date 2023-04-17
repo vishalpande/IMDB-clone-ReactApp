ea663371dd3e2534fc8a4160a73b689610cbefac
vishalpande <pandevishal40@gmail.com>
2023-04-17 18:30:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import './App.css';
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-
-
+import Header from './components/header/Header';
+import Home from './components/pages/Home';
 function App() {
   return (
 
 
 <div className="App">
 <Router>
+  <Header/>
   <Routes>
-<Route index element={<h1>hello world</h1>}></Route>
+<Route index element={<Home/>}></Route>
 <Route path="movie/:id" element={<h1>movies detail</h1>}></Route>
-<Route path="movie/:types" element={<h1>movies list page</h1>}></Route>
+<Route path="movies/:types" element={<h1>movies list page</h1>}></Route>
 <Route path="/*" element={<h1>Error page</h1>}></Route>
 </Routes>
 </Router>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,54 @@
+import React, { useEffect,useState } from 'react'
+import "./Home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
+
+
+export default function Home() {
+ 
+const [popularMovies,setpopularMovies]=useState([])
+
+
+    useEffect(()=>{
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+.then(res => res.json())
+.then(data => setpopularMovies(data.results))
+},[])
+
+return (
+    <>
+    <div className="poster">
+        <Carousel
+            showThumbs={false}
+            autoPlay={true}
+            transitionTime={3}
+            infiniteLoop={true}
+            showStatus={false}
+        >
+            {
+                popularMovies.map(movie => (
+                    <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
+                        <div className="posterImage">
+                            <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                        </div>
+                        <div className="posterImage__overlay">
+                            <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+                            <div className="posterImage__runtime">
+                                {movie ? movie.release_date : ""}
+                                <span className="posterImage__rating">
+                                    {movie ? movie.vote_average :""}
+                                    <i className="fas fa-star" />{" "}
+                                </span>
+                            </div>
+                            <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                        </div>
+                    </Link>
+                ))
+            }
+        </Carousel>
+        
+    </div>
+</>
+)
+}
