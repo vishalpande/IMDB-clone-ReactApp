ef5f547dd9e8ea775701e9365e3f3d63abc6b6de
vishalpande <pandevishal40@gmail.com>
2023-04-17 16:43:05 +0530

    Updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+
 
 function App() {
   return (
 
-<div>
-  hello
+
+<div className="App">
+<Router>
+  <Routes>
+<Route index element={<h1>hello world</h1>}></Route>
+<Route path="movie/:id" element={<h1>movies detail</h1>}></Route>
+<Route path="movie/:types" element={<h1>movies list page</h1>}></Route>
+<Route path="/*" element={<h1>Error page</h1>}></Route>
+</Routes>
+</Router>
 </div>
 
   );
